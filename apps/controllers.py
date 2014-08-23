11ab70eaf93d34a550a704bfb7701c2b9bbdddda
Young-Ho Cha <ganadist@gmail.com>
2014-08-23 20:29:59 +0900

    controllers: add parameters on each pages
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- apps/controllers.py
+++ apps/controllers.py
@@ -10,32 +10,38 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/create', methods=['GET'])
+@app.route('/create', methods=['GET', 'POST'])
 def create():
 
     return render_template('create.html')
 
-@app.route('/detail', methods=['GET'])
-def detail():
+@app.route('/detail/<int:quest_id>', methods=['GET', 'POST'])
+@app.route('/detail/<int:quest_id>/', methods=['GET', 'POST'])
+@app.route('/detail/<int:quest_id>/<int:user_id>', methods=['GET', 'POST'])
+def detail(quest_id, user_id = None):
 
     return render_template('detail.html')
 
 @app.route('/list', methods=['GET'])
-def list():
-
+@app.route('/list/', methods=['GET'])
+@app.route('/list/<mode>', methods=['GET'])
+@app.route('/list/<mode>/', methods=['GET'])
+@app.route('/list/<mode>/<int:page>', methods=['GET'])
+def list(mode = 'joined', page = 0):
+    assert(mode in ('joined', 'created'))
     return render_template('list.html')
 
-@app.route('/manage', methods=['GET'])
-def manage():
+@app.route('/manage/<int:quest_id>', methods=['GET', 'POST'])
+def manage(quest_id):
 
     return render_template('manage.html')
 
-@app.route('/review', methods=['GET'])
-def review():
-
-    return render_template('review.html')
-
 @app.route('/user', methods=['GET'])
-def user():
+@app.route('/user/', methods=['GET'])
+@app.route('/user/<int:user_id>', methods=['GET'])
+def user(user_id = None):
+    if not user_id:
+        # get user_id from session
+        pass
 
     return render_template('user.html')
