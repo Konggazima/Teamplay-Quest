bada12229eb8d2ac25f6677c827dce36a5860dda
KBuild <qwer7995@gmail.com>
2014-08-24 05:09:24 +0900

    Group Create Patch

--- apps/static/js/group_create.js
+++ apps/static/js/group_create.js
@@ -11,18 +11,35 @@ $(document).ready(function () {
 		$("#friends input").each( function(e) {
 			if( e.checked ) {
 				dom = "<img src=\"\" width=\"50px\" height=\"50px\" />";
-				$("#add-friends").before(dom);	
+				$("#add-friends").before(dom);
 			}
 		});
+		closeAddFriendsPopup();
+	});
+
+	#(".btn-circle").click( function() {
+		checked = this.getAtrribute('data-checked');
+
+		if( checked ) {
+			$(this).removeClass("glyphicon glyphicon-remove-circle");
+			$(this).addClass("glyphicon glyphicon-ok-circle");
+		} else {
+			$(this).removeClass("glyphicon glyphicon-ok-circle");
+			$(this).addClass("glyphicon glyphicon-remove-circle");
+		}
+
+		this.setAttribute('data-checked', !checked);
 	});
 });
 
 function openAddFriendsPopup() {
-	$("#add-friends-popup").css({"display": "block", "z-index": 2});
-	$("#whiteboard").css({"display": "block", "z-index": 1});
+	$(document).css({z-index, -1});
+	$("#add-friends-popup").css({"display": "block", "z-index": 1});
+	$("#whiteboard").css({"display": "block", "z-index": 0});
 }
 
 function closeAddFriendsPopup() {
+	$(document).css({z-index, 0});
 	$("#add-friends-popup").css({"display": "none", "z-index": -1});
 	$("#whiteboard").css({"display": "none", "z-index": -1});	
 }
