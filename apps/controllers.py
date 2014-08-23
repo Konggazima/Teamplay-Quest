ef7bb90eaa2faaa0b5172a5b3e71b190bf6cad78
Young-Ho Cha <ganadist@gmail.com>
2014-08-23 19:31:58 +0900

    add templates
    Just copy from index.html
    
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- apps/controllers.py
+++ apps/controllers.py
@@ -12,3 +12,33 @@ from apps.models import (
 def index():
 
     return render_template('index.html')
+
+@app.route('/create', methods=['GET'])
+def create():
+
+    return render_template('create.html')
+
+@app.route('/detail', methods=['GET'])
+def detail():
+
+    return render_template('detail.html')
+
+@app.route('/list', methods=['GET'])
+def list():
+
+    return render_template('list.html')
+
+@app.route('/manage', methods=['GET'])
+def manage():
+
+    return render_template('manage.html')
+
+@app.route('/review', methods=['GET'])
+def review():
+
+    return render_template('review.html')
+
+@app.route('/user', methods=['GET'])
+def user():
+
+    return render_template('user.html')
